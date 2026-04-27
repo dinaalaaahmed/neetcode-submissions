8205af3dd0875f51b57d90074a6d1e3cbf9728af
Dina Alaa <42760885+dinaalaaahmed@users.noreply.github.com>
2026-04-27 15:31:38 +0300

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int MaxArea(int[] heights) {
+        int startIndex = 0;
+        int lastIndex = heights.Length - 1;
+        int maxArea = 0;
+        while (lastIndex > startIndex){
+            int area = (lastIndex-startIndex)*Math.Min(heights[startIndex], heights[lastIndex]);
+            if(area > maxArea){
+                maxArea = area;
+            }
+            if(heights[startIndex] < heights[lastIndex]){
+                startIndex++;
+            }
+            else {
+                lastIndex--;
+            }
+        }
+        return maxArea;
+    }
+}
